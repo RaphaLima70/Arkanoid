e411386699373d059ad0dc69ca35b5cdd97b5263
Raphael Albino <raphaelalbino_@hotmail.com>
2020-06-16 21:08:14 -0300

    Upando a base do projeto

---
+++ Assets/Scripts/Bola.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+    public Vector2 velocidade;
+    void Start()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.AddForce(velocidade);
+
+    }
+
+   
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public float velocidade = 10.0f;
+    public float horizontalAxis;
+    public Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+    void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(horizontalAxis * velocidade,0);
+
+    }
+}
